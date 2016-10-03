54094ca847ae6fa1abf1ba2baa5a2dec6aa523cc
Aathira13 <koaathira@gmail.com>
2016-10-04 00:40:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text'
 element.innerhTML = 'New value';
 
 //move the image
-var img = document.getElementbyId('img');
+var img = document.getElementbyId('madi');
 img.onclick = function () {
   img.style.marinleft ='100px';  
 };
